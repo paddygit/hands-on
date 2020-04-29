359b78ea6fc9c82f6d64a89d941445a62bb1144f
paddygit <psone@live.com>
2020-04-29 20:42:26 +0530

    JS02 let and const

---
+++ JS/Learning_Dev_Ed/JS_S1/JS02/variables.js
@@ -0,0 +1,36 @@
+// let and const
+
+// assign 100 to inbox
+// let inbox1 = 100;
+// let inbox2 = 265;
+//assign paddy to user
+// let user = "paddy";
+
+// alert(inbox + 1);
+
+// let allEmails = inbox1 + inbox2;
+// alert(allEmails);
+// ------------------------
+// let inbox = 100;
+
+// inbox = 250;
+
+// alert(inbox);
+// ---------------------------
+
+// let inbox;
+// inbox = 100;
+// console.log(inbox);
+
+// ---------------------------
+// const inbox; ERROR!!!
+
+// const inbox = 100; //cannot change
+// inbox = 50;
+// console.log(inbox);
+
+// let 1user = "Paddy"; ERROR!!!
+// let let = ... //ERROR!!!
+
+//camelCase for readability
+//let mailNumber = 5;

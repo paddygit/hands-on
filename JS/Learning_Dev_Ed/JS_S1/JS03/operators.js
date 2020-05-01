d7c4bc7ccf3efb6a2681217fc7f6ca7d6b28cf40
paddygit <psone@live.com>
2020-05-01 15:51:37 +0530

    JS03 Operators

---
+++ JS/Learning_Dev_Ed/JS_S1/JS03/operators.js
@@ -0,0 +1,19 @@
+//JavaScript Operators
+
+//Basic Operators
+
+let inbox = 10;
+
+console.log(inbox + 5);
+console.log(inbox - 5);
+console.log(inbox * 5);
+console.log(inbox / 5);
+console.log(inbox ** 2);
+console.log(inbox ** 3);
+console.log(inbox % 3);
+console.log(inbox++);
+console.log(inbox--);
+console.log(--inbox);
+console.log(++inbox);
+inbox += 20; // same as inbox = inbox + 20
+console.log(inbox);

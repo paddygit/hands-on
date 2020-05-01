285cd4808b7c5a03fa2fd152718e2041413f1391
paddygit <psone@live.com>
2020-05-01 16:23:11 +0530

    JS04 Concatenation

---
+++ JS/Learning_Dev_Ed/JS_S1/JS04/concatenation.js
@@ -0,0 +1,39 @@
+// const bank1 = 100;
+// const bank2 = 50;
+
+// const allBanks = bank1 + bank2;
+
+// console.log(allBanks);
+// ----
+
+// const greeting = "Welcome to our website "; //SPACE AFTER THE LAST WORD
+// const user = "Paddy";
+
+// console.log(greeting +user);
+
+// ----
+// const greeting = "Welcome to our website";
+// const user = "Paddy";
+
+// console.log(greeting + " " + user); //SPACE IN THE CONCATENATION
+
+// -----
+// const greeting = "Simon says";
+// const user = "\"it\'s easy\"";
+
+// console.log(greeting + " " + user);
+
+// ------
+// const greeting = "Simon says";
+// const user = '"it\'s easy"';
+
+// console.log(greeting + " " + user);
+
+// --------------------This doesn't work easy on my system
+// const user = "Paddy";
+// const greeting = "${user}";
+
+// console.log(greeting);
+// ------------
+
+// 1+'1' will give "11" as a String. Kind of wierd but this is the behaviour of JS
